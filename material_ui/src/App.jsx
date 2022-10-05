8307c3f98aafa23611566cf1b75f71e99e1e14a1
perez1187 <o.perez1187@gmail.com>
2022-10-05 16:43:26 +0200

    some btn etc

--- material_ui/src/App.jsx
+++ material_ui/src/App.jsx
@@ -2,22 +2,64 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from '@mui/material/Button';
-import Navbar from './components/Navbar/Navbar';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material';
 
-function App() {
 
-
+function App () {
   return (
-    <> 
-    <Button 
-    variant="contained"
-    // disabled event will not work
-    onClick={() => console.log('click event')}
-    >
-      Hello World</Button>
-      <Navbar />
-    </>
+    <h1> chujs</h1>
   )
 }
+
+// under example of using material ue
+
+// function App() {
+
+//   const BlueButton = styled (Button)({backgroundColor:"red",
+//   "&:hover":{
+//     backgroundColor: "blue"
+//   },
+//   "&:disabled":{
+//     backgroundColor: "gray"
+//   }})
+
+//   return (
+//     <> 
+//     <Button 
+//     variant="contained"
+//     // disabled event will not work
+//     color='secondary'
+//     size='small'
+//     startIcon={<SettingsIcon />}
+//     onClick={() => console.log('click event')}
+//     >
+//       Hello World</Button>
+//     <Typography variant="h1" component="p">
+//       iT USES H1 style BUT IT IS p 
+//     </Typography>;
+    
+//     {/* this is how we can create custom button 
+//       sx allows custom css style
+//     */}
+//     <Button 
+//       variant='contained'
+//       // disabled // disbaled css only when disabled Treu
+//       sx={{
+//         backgroundColor:"red",
+//         "&:hover":{
+//           backgroundColor: "blue"
+//         },
+//         "&:disabled":{
+//           backgroundColor: "gray"
+//         }
+//       }}
+//     > My unique button </Button>
+//     <BlueButton> my custom button</BlueButton>
+   
+//     </>
+//   )
+// }
 
 export default App
---
+++ material_ui/src/theme.jsx
@@ -0,0 +1,6 @@
+import { createTheme } from "@mui/material";
+
+
+// here we can creatae our own shapes
+// you need to wrap what you want to use in theme to change it https://www.youtube.com/watch?v=fzxEECHnsvU
+export const theme = createTheme({})
