1b9463f902ec6f7d0b0cad783ad1f4a2aa4ff8f2
perez1187 <o.perez1187@gmail.com>
2022-10-05 16:00:48 +0200

    two

--- material_ui/src/App.jsx
+++ material_ui/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from '@mui/material/Button';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
 
   return (
+    <> 
     <Button 
     variant="contained"
-    // disabled
+    // disabled event will not work
     onClick={() => console.log('click event')}
     >
       Hello World</Button>
+      <Navbar />
+    </>
   )
 }
 
